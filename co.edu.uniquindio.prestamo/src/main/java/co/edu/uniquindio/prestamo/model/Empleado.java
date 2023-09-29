9c7a48231ac80956b09a32fcd0996ae6dc54d22e
JaviLeyton <oscarj.leytono@uqvirtual.edu.co>
2023-09-28 19:13:17 -0500

    Creación clase empleado

---
+++ co.edu.uniquindio.prestamo/src/main/java/co/edu/uniquindio/prestamo/model/Empleado.java
@@ -0,0 +1,63 @@
+package co.edu.uniquindio.prestamo.model;
+
+public class Empleado {
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private double edad;
+    PrestamoUq ownedByPrestamoUq;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String apellido, String cedula, double edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getCedula() {
+        return cedula;
+    }
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    public double getEdad() {
+        return edad;
+    }
+    public void setEdad(double edad) {
+        this.edad = edad;
+    }
+
+    public PrestamoUq getOwnedByPrestamoUq() {
+        return ownedByPrestamoUq;
+    }
+
+    public void setOwnedByPrestamoUq(PrestamoUq ownedByPrestamoUq) {
+        this.ownedByPrestamoUq = ownedByPrestamoUq;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", cedula='" + cedula + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+
+}
