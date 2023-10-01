cba9cda4d42e706579065773de6dd6b3c9bb759e
JaviLeyton <oscarj.leytono@uqvirtual.edu.co>
2023-10-01 18:40:43 -0500

    :+1:  creacion clase prestamo

--- co.edu.uniquindio.prestamo/src/main/java/co/edu/uniquindio/prestamo/Main.java
+++ co.edu.uniquindio.prestamo/src/main/java/co/edu/uniquindio/prestamo/Main.java
@@ -3,6 +3,7 @@ package co.edu.uniquindio.prestamo;
 import co.edu.uniquindio.prestamo.model.Cliente;
 import co.edu.uniquindio.prestamo.model.PrestamoUq;
 import co.edu.uniquindio.prestamo.model.Empleado;
+import co.edu.uniquindio.prestamo.model.Prestamo;
 
 import java.util.List;
 
@@ -22,18 +23,60 @@ public class Main {
         crearEmpleado("Jerónimo", "Osorio", "1099", 26, prestamoUq);
         crearEmpleado("Augusto", "Álvarez", "1001", 54, prestamoUq);
         crearEmpleado("Camilo", "Marulanda", "1002", 18, prestamoUq);
-        System.out.println("--------> Lista de clientes <-------");
+
+        // CREAR PRESTAMOS
+        crearPrestamo(10,
+                "Marzo 20 de 2023",
+                "Abril 14 de 2023",
+                "Prestamo de palas", prestamoUq);
+        crearPrestamo(11,
+                "Marzo 25 de 2023",
+                "Abril 30 de 2023",
+                "Prestamo de baldes", prestamoUq);
+        crearPrestamo(12,
+                "Enero 03 de 2023",
+                "Mayo 15 de 2023",
+                "Prestamo de martillo", prestamoUq);
+        crearPrestamo(13,
+                "Junio 06 de 2023",
+                "Agosto 29 de 2023",
+                "Prestamo de destornilladores", prestamoUq);
+        crearPrestamo(14,
+                "Febrero 19 de 2023",
+                "Febero 27 de 2023",
+                "Prestamo de andamios", prestamoUq);
+        crearPrestamo(15,
+                "Agosto 26 de 2023",
+                "Septiembre 28 de 2023",
+                "Prestamo de mezcladora", prestamoUq);
+
+        System.out.println("\n--------> Lista de clientes <-------\n");
         mostrarInformacionClientes(prestamoUq);
         eliminarCliente(prestamoUq, "1095");
-        System.out.println("--------> Lista de clientes después de eliminar cliente <-------");
+        System.out.println("\n--------> Lista de clientes después de eliminar cliente <-------\n");
         mostrarInformacionClientes(prestamoUq);
-        System.out.println("--------> Lista de empleados <-------");
+
+        System.out.println("\n--------> Lista de empleados <-------\n");
         mostrarInformacionEmpleados(prestamoUq);
         eliminarEmpleado(prestamoUq, "1095");
-        System.out.println("--------> Lista de empleados después de eliminar empleado <-------");
+        System.out.println("\n--------> Lista de empleados después de eliminar empleado <-------\n");
         mostrarInformacionEmpleados(prestamoUq);
+
+        System.out.println("\n--------> Lista de prestamos <-------\n");
+        mostrarInformacionPrestamos(prestamoUq);
+        eliminarPrestamo(prestamoUq, 10);
+        System.out.println("\n--------> Lista de prestamos después de eliminar un prestamo <-------\n");
+        mostrarInformacionPrestamos(prestamoUq);
     }
 
+    /**
+     * Método de para crear cliente
+     * @param nombre
+     * @param apellido
+     * @param cedula
+     * @param edad
+     * @param prestamoUq
+     */
     private static void crearCiente(String nombre,
                                     String apellido,
                                     String cedula,
@@ -42,6 +85,14 @@ public class Main {
         prestamoUq.crearCliente(nombre, apellido, cedula, edad);
     }
 
+    /**
+     * Método para crear empleado
+     * @param nombre
+     * @param apellido
+     * @param cedula
+     * @param edad
+     * @param prestamoUq
+     */
     private static void crearEmpleado(String nombre,
                                     String apellido,
                                     String cedula,
@@ -50,6 +101,26 @@ public class Main {
         prestamoUq.crearEmpleado(nombre, apellido, cedula, edad);
     }
 
+    /**
+     * Método para crear Prestamo
+     * @param numeroPrestamo
+     * @param fechaPrestamo
+     * @param fechaEntrega
+     * @param descripcion
+     * @param prestamoUq
+     */
+    private static void crearPrestamo(int numeroPrestamo,
+                                      String fechaPrestamo,
+                                      String fechaEntrega,
+                                      String descripcion,
+                                      PrestamoUq prestamoUq) {
+        prestamoUq.crearPrestamo(numeroPrestamo, fechaPrestamo, fechaEntrega, descripcion);
+    }
+
+    /**
+     * Método para mostrar la información de clientes.
+     * @param prestamoUq
+     */
     private static void mostrarInformacionClientes(PrestamoUq prestamoUq) {
         List<Cliente> listaCliente = prestamoUq.obtenerClientes();
         int tamanioLista = listaCliente.size();
@@ -59,6 +130,10 @@ public class Main {
         }
     }
 
+    /**
+     * Método para mostrar información de empleados
+     * @param prestamoUq
+     */
     private static void mostrarInformacionEmpleados(PrestamoUq prestamoUq) {
         List<Empleado> listaEmpleado = prestamoUq.obtenerEmpleados();
         int tamanioLista = listaEmpleado.size();
@@ -68,14 +143,50 @@ public class Main {
         }
     }
 
+    /**
+     * Método para mostrar información de prestamos
+     * @param prestamoUq
+     */
+    private static void mostrarInformacionPrestamos(PrestamoUq prestamoUq) {
+        List<Prestamo> listaPrestamo = prestamoUq.obtenerPrestamos();
+        int tamanioLista = listaPrestamo.size();
+        for(int i = 0; i < tamanioLista; i++){
+            Prestamo prestamo = listaPrestamo.get(i);
+            System.out.println(prestamo.toString());
+        }
+    }
+
+    /**
+     * Método para emiliar un cliente
+     * @param prestamoUq
+     * @param cedula
+     */
     private static void eliminarCliente(PrestamoUq prestamoUq, String cedula) {
         prestamoUq.eliminarCliente(cedula);
     }
 
+    /**
+     * Método para eliminar un empleado.
+     * @param prestamoUq
+     * @param cedula
+     */
     private static void eliminarEmpleado(PrestamoUq prestamoUq, String cedula) {
         prestamoUq.eliminarEmpleado(cedula);
     }
 
+    /**
+     * Método para eliminar un prestamo.
+     * @param prestamoUq
+     * @param numeroPrestamo
+     */
+    private static void eliminarPrestamo(PrestamoUq prestamoUq, int numeroPrestamo) {
+        prestamoUq.eliminarPrestamo(numeroPrestamo);
+    }
+
+    /**
+     * Método para inicializar datos prueba.
+     * @return
+     */
     private static PrestamoUq inicializarDatosPrueba() {
         PrestamoUq prestamoUq = new PrestamoUq();
         prestamoUq.setNombre("Prestamo rapido");
